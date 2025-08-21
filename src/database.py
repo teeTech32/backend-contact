741256a3607baa112bf5ef2bd110ef5782d08ecb
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2025-08-21 06:34:20 +0100

    database.py

--- src/database.py
+++ src/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = 'postgresql://contact_progresdb_user:JTOnzXib4ppVN5HCuxD8TfGBUhkonl5t@dpg-d1qh8hidbo4c73ch4f70-a/contact_progresdb'
+DATABASE_URL = 'postgresql://contactadb_user:5CdljSJIDAI5dSo6ATsuqwGTA2MymVzx@dpg-d2is7aemcj7s73cqdu4g-a/contactadb'
 
 engine = create_engine(DATABASE_URL)
 
